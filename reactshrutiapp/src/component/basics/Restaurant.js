3487ca02571201140dc3134984b0776ee5c3a859
Shruti Tiwari <sh03shruti@gmail.com>
2021-11-16 19:11:58 +0530

    restaurant site

---
+++ reactshrutiapp/src/component/basics/Restaurant.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.css";
+
+const Restaurant = () => {
+  return (
+    <>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-body">
+            <span className="card-number card-circle subtle">1</span>
+            <span className="card-author subtle">Breakfast</span>
+            <h2 className="card-title">Maggi</h2>
+            <span classNmae="card-description subtle">
+              Maggi is an international brand of seasonings, instant soups, and
+              noodles that originated in Switzerland in the late 19th century.
+              The Maggi company was acquired by Nestlé in 1947.
+            </span>
+            <div className="card-read">Read</div>
+          </div>
+          <img src={image} alt="images" className="card-media"></img>
+          <span className="card-tag subtle">Order Now!</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Restaurant;
