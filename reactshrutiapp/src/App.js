998e3c2fdd9ce6bcd15c4806eadf3caa89dfa044
Shruti Tiwari <sh03shruti@gmail.com>
2021-11-17 17:13:50 +0530

    useReducer Hook

--- reactshrutiapp/src/App.js
+++ reactshrutiapp/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 // import Restaurant from "./component/basics/Restaurant";
 // import UseState from "./component/Hooks/UseState";
-import UseEffect from "./component/Hooks/useEffect";
+// import UseEffect from "./component/Hooks/UseEffect";
+import UseReducer from "./component/Hooks/UseReducer";
 
 const App = () => {
   // return <Restaurant />;
   // return <UseState />;
-  return <UseEffect />;
+  // return <UseEffect />;
+  return <UseReducer />;
 };
 
 export default App;
